16e480b08d324d7f19731d132697415b76dd7fd0
Jorge Luis <jorgestudio2017@gmail.com>
2024-11-28 22:34:00 -0400

    fix: base api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-blog-techtalk.onrender.com',
+  apiUrl: 'https://api-blog-techtalk.onrender.com/api',
   basePath:'https://api-blog-techtalk.onrender.com',
   bucketS3: 'blog-front-prod',
   version:'v1.0.0'
