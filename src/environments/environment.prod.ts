dfd3b4bd61b3f5737ea1d7456121fbd254504998
Jorge Luis <jorgestudio2017@gmail.com>
2024-11-28 22:04:17 -0400

    fix: modificar las url del API para prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.techblog.dev',
-  basePath:'https://api.techblog.dev',
+  apiUrl: 'https://api-blog-techtalk.onrender.com',
+  basePath:'https://api-blog-techtalk.onrender.com',
   bucketS3: 'blog-front-prod',
   version:'v1.0.0'
 };
