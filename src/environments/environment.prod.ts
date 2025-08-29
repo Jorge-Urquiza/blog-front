656e6403bdf59e227d0b466d8fe8ec2700b2c52e
Jose Andres Mayser Vasquez <joseandresmayser@gmail.com>
2025-08-28 22:35:07 -0400

    chore: Localhost Backend url was added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.techblog.dev',
-  basePath:'https://api.techblog.dev',
+  apiUrl: 'http://localhost:3000/api',
+  basePath:'http://localhost:3000/api',
   bucketS3: 'blog-front-prod',
   version:'v1.0.0'
 };
